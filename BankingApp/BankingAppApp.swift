c94d2b1f7014fb1d33a1c593a5fc1b7b471eea9b
totan68 <164209696+totan68@users.noreply.github.com>
2024-08-06 21:32:11 +0530

    Initial Commit

---
+++ BankingApp/BankingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingAppApp.swift
+//  BankingApp
+//
+//  Created by Totan Hazra on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BankingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
